3239c8f674edd62f31d0be1153607a1b4138fb19
ShailajDahal-khwopa <kce080bct036@khwopa.edu.np>
2025-05-17 09:34:49 +0545

    created a TCP listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::net::TcpListenter;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection established!");
+    }
 }
